591596a12bfb3fd763d09d57ca1a022b1fe0ab59
Indraji Wanigaratne <indrajiwanigaratne@192.168.1.3>
2025-06-17 03:12:20 +0530

    Tweaks

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import sys
 
 def main():
+
+    input_text = sys.argv[1]
+
+    placeholder = "Placeholder"
+
+    print(placeholder)
     
-    return sys.argv[1]
+    return placeholder
 
 if __name__ == "__main__":
     result = main()
